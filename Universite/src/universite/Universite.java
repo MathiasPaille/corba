951a120d4921fa9568e0c93ef482b67271899ab4
MathiasPaille <mathias.paille@gmail.com>
2015-06-11 17:49:08 +0200

    dlzpâ
    Debut implet metier ça dechire

--- Universite/src/universite/Universite.java
+++ Universite/src/universite/Universite.java
@@ -1,11 +1,38 @@
 package universite;
 
+import gestionVoeu.*;
+import org.omg.CosNaming.*;
+import org.omg.CosNaming.NamingContextPackage.*;
+import org.omg.CORBA.*;
+import org.omg.PortableServer.*;
+import org.omg.PortableServer.POA;
+import java.util.Properties;
+import org.omg.CORBA.SystemException;
 /**
  *
  * @author Yvan
  */
-public class Universite {
 
+//IMPLEMENTATION DE LA CLASSE MINISTERE POA /!\ CODE METIER
+class UniversiteImpl extends UniversitePOA {
+        private ORB orb;
+
+        public void setORB(ORB orb_val) {
+            orb = orb_val;
+        }
+
+        @Override
+        public CandidatureDetail[] consulterCandidatures() {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        @Override
+        public void modifierCandidature(CandidatureDetail candidature, EtatDecision etatDecision) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        } 
+}
+
+public class Universite {   
     /**
      * @param args the command line arguments
      */
