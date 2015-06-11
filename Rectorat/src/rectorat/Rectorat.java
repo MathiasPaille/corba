6fac03a69d0d291e7a17619e1851b222369627c3
MathiasPaille <mathias.paille@gmail.com>
2015-06-11 17:23:24 +0200

    feafa
    recotrat

--- Rectorat/src/rectorat/Rectorat.java
+++ Rectorat/src/rectorat/Rectorat.java
@@ -1,9 +1,33 @@
 package rectorat;
 
+import gestionVoeu.*;
+import org.omg.CosNaming.*;
+import org.omg.CosNaming.NamingContextPackage.*;
+import org.omg.CORBA.*;
+import org.omg.PortableServer.*;
+import org.omg.PortableServer.POA;
+import java.util.Properties;
 /**
  *
- * @author Yvan
+ * @author Mathias
  */
+class RectoratImpl extends RectoratPOA {
+    private ORB orb;
+    
+    public void setORB(ORB orb_val) {
+        orb = orb_val;
+    }
+    
+    //implement creerVoeux method
+    public void creerVoeux(VoeuxDetail monVoeux) {
+        
+    }
+    //implement  method
+    //implement  method
+    //implement  method
+    //implement  method    
+}
+
 public class Rectorat {
 
     /**
